c7500e4987d035985a79551adb788553e24bf372
LahiruHarshana <lharshana2002@gmail.com>
2023-12-01 09:40:48 +0530

    updated

--- src/main/java/lk/ijse/gdse66/hellojavaee3/HelloServlet.java
+++ src/main/java/lk/ijse/gdse66/hellojavaee3/HelloServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello servlet");
 
     }
 }
