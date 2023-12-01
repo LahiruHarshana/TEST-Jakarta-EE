a8a7a8460cbcbe272de9c27e7e23502a667d9708
LahiruHarshana <lharshana2002@gmail.com>
2023-12-01 09:36:58 +0530

    updated

---
+++ src/main/java/lk/ijse/gdse66/hellojavaee3/HelloServlet.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.hellojavaee3;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
